210a12c2e682f928a7e03377f5036982c70d854d
ilikeevb <ilikeevb@yandex.ru>
2025-09-23 16:32:15 +0500

    feat: setAssetsPath

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import { SnowfallIcon } from 'react-library';
 
 function App() {
+  const [iconName, setIconName] = useState('fish');
+
   return (
     <div className="App">
-      <SnowfallIcon name='fish' />
+      <SnowfallIcon name={iconName} />
+      <input
+        value={iconName}
+        onChange={e => setIconName(e.target.value)}
+      />
     </div>
   );
 }
--- packages/react-library/lib/index.ts
+++ packages/react-library/lib/index.ts
@@ -1,5 +1,5 @@
 import { setAssetPath } from 'stencil-library/dist/components/index.js';
 
-setAssetPath(new URL('../stencil-library/dist/stencil-library/', import.meta.url).href);
+setAssetPath(new URL('../../stencil-library/dist/stencil-library/dist/', import.meta.url).href);
 
 export * from './components/stencil-generated/components';
