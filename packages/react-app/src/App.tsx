bd0979a6371a9c41270bc1411bd3b7ddb1539dca
ilikeevb <ilikeevb@yandex.ru>
2025-10-05 12:40:11 +0500

    fix: app.tsx

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -1,17 +1,10 @@
-import { useState } from 'react';
 import './App.css';
 import { SnowfallIcon } from 'react-library';
 
 function App() {
-  const [iconName, setIconName] = useState('fish');
-
   return (
     <div className="App">
-      <SnowfallIcon name={iconName} />
-      <input
-        value={iconName}
-        onChange={e => setIconName(e.target.value)}
-      />
+      <SnowfallIcon name="fish" size={64} />
     </div>
   );
 }
