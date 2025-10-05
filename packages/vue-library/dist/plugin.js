8e2d6479cbdb3808dc819d9611a080527c4987d9
Bilal <41923688+ilikeevb@users.noreply.github.com>
2025-10-05 19:30:13 +0500

    Fix Stencil icon assets for Vue app

--- packages/vue-library/dist/plugin.js
+++ packages/vue-library/dist/plugin.js
@@ -1,6 +1,8 @@
-import { defineCustomElements, } from "stencil-library/loader";
+import { defineCustomElements, setAssetPath } from "stencil-library/loader";
+const ASSET_PATH = new URL("../stencil-library/dist/", import.meta.url).href;
 export const ComponentLibrary = {
     install() {
+        setAssetPath(ASSET_PATH);
         defineCustomElements();
     },
 };
